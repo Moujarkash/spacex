90861a17e5f8c681bc43aba2a9e1c79dd2a31aca
Mod <mouhmmad.moujarkash@medicus.ai>
2020-12-04 16:57:44 +0200

    - handled the navigation to the screens and implemented the home page

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,8 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:spacex/view/pages/UpcomingLaunchesPage.dart';
+import 'package:spacex/view/pages/next_launch_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _children = List<Widget>();
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    _children.add(NextLaunchPage());
+    _children.add(UpcomingLaunchesPage());
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+          body: _children[_currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            currentIndex: _currentIndex,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.timer),
+                  label: ''
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.list),
+                  label: ''
+              )
+            ],
+          )),
+    );
   }
 }
